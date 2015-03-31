893b3ca71a10efd74a1beef5ff38e6dcd299deab
Steve Hatchard <steve@code.je>
2015-03-31 07:48:16 +0100

    Changed providers check to codentia

--- src/app/Providers/CESqlMembershipProvider.cs
+++ src/app/Providers/CESqlMembershipProvider.cs
@@ -72,7 +72,7 @@ namespace Codentia.Common.Membership.Providers
 
                 for (int i = 0; i < membership.Providers.Count; i++)
                 {
-                    if (membership.Providers[i].Type.ToLower().StartsWith("mattchedit"))
+                    if (membership.Providers[i].Type.ToLower().StartsWith("codentia"))
                     {
                         providerSettings = membership.Providers[i];
                     }
